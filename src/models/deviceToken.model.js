2fabc040f90a16b7b306bbd641458e89801ed595
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-07-07 16:20:45 +0700

    add device token models

---
+++ src/models/deviceToken.model.js
@@ -0,0 +1,53 @@
+const db = require("../helpers/db.helper")
+
+const table = "deviceToken"
+exports.findAll = async function (page, limit, sort, sortBy) {
+  page = parseInt(page) || 1
+  limit = parseInt(limit) || 5
+  sort = sort || "id"
+  sortBy = sortBy || "ASC"
+
+  const offset = (page - 1) * limit
+
+  const query = ` 
+  SELECT * FROM "${table}" 
+    ORDER BY "${sort}" ${sortBy} 
+    LIMIT $1 OFFSET $2`
+
+  const values = [limit, offset]
+
+  const { rows } = await db.query(query, values)
+  return rows
+}
+
+exports.insertToken = async function (id, data) {
+  const query = `
+  INSERT INTO "${table}" ("token", "userId")
+  VALUES ($1, $2) RETURNING *
+  `
+  const values = [data.token, id]
+
+  const { rows } = await db.query(query, values)
+  return rows[0]
+}
+exports.findOneByToken = async (token) =>{
+  const query = `
+  SELECT * FROM "${table}"
+  WHERE token = $1
+  `
+  const values = [token]
+
+  const { rows } = await db.query(query, values)
+  return rows[0]
+}
+exports.updateUSerIdByToken = async (token, id) =>{
+  const query = `
+  UPDATE"${table}"
+  SET "userId"=$2
+  WHERE token = $1
+  `
+  const values = [token, id]
+
+  const { rows } = await db.query(query, values)
+  return rows[0]
+}
