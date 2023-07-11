70b54e9a1f78f3b3c7b7b4a426d8254c0af0d0c6
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-07-11 13:40:32 +0700

    add env firebase

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -1,5 +1,6 @@
 var admin = require("firebase-admin")
-var serviceAccount = require("../../event-app-e11a2-firebase-adminsdk-72mau-9bcb1dad94.json")
+// var serviceAccount = require("../../event-app-e11a2-firebase-adminsdk-72mau-9bcb1dad94.json")
+var serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS)
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
