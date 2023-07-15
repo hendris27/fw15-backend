aa5aecce8831e3979503634710862d24c8ec7421
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-07-15 08:35:53 +0700

    add wishlist status

--- src/routers/wishlist.router.js
+++ src/routers/wishlist.router.js
@@ -4,6 +4,7 @@ const validate = require("../middlewares/validator.middleware")
 const wishlistController = require("../controllers/wishlist.controller")
 
 wishlistRouter.get("/", wishlistController.getWishlist)
+wishlistRouter.get("/:id", wishlistController.getDetailWishlist)
 wishlistRouter.post("/", validate("addWishlist"),wishlistController.createWishlist)
 wishlistRouter.delete("/managedeleted/:id", wishlistController.delWishlist)
 
