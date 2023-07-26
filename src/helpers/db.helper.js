5e6660d4cc4db74a1abb29ec0aab6b0cfe862d63
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-07-26 20:52:52 +0700

    fix

--- src/helpers/db.helper.js
+++ src/helpers/db.helper.js
@@ -1,14 +1,14 @@
 const {Pool} =require("pg")
 
 const db = new Pool({
-    connectionString:process.env.DATABASE
+  connectionString:process.env.DATABASE
 })
 
 db.connect().then(()=>{
-    console.log("Database connected")
+  console.log("Database connected")
 }).catch((err)=>{
-    console.log(err)
-    console.log("Failed to connected Database!")
+  console.log(err)
+  console.log("Failed to connected Database!")
 })
 
 module.exports= db
